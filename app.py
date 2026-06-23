cd39097fc8af60258ac193556ea95848d2eda95f
riinno <bryno.bdx1@gmail.com>
2026-06-23 18:02:42 -0300

    Creating app.py and requirements

---
+++ app.py
@@ -0,0 +1,9 @@
+# by Riinno, 2026
+
+# -----------------------------------------
+# Importação de dependencias e status base
+
+from flask import Flask
+
+app = Flask(__name__)
+# -----------------------------------------
